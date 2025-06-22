b041c7072512fd502e6acd01dbd06ca1b9c5bfac
muho <mh.jung.dev@gmail.com>
2025-06-22 21:35:18 +0900

    init

---
+++ src/main/java/com/homae/HomaeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.homae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomaeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomaeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/homae/HomaeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomaeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
